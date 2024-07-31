d9f6648b63e54fc7ed37709eff3f4b9fc2a5b81a
jpillaca <jpillaca@quimicasuiza.com>
2024-07-31 11:11:46 -0500

    ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,23 @@
-export const environment = {};
+const urlApiReportes = '#{urlApiReportes}';
+const urlApiMq = '#{urlApiMq}';
+const tokenMq = '#{tokenMq}';
+const TOKEN =
+  'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJxdWltaXZlbnRhIiwiaGFtYmllbnRlIjoicHJkIiwieWVhciI6IjIwMTkiLCJzdXBwb3J0Ijoid3d3LnF1aW1pY2FzdWl6YS5jb20ifQ.aivnszuHEsXVxM1BWUy4uHhbS46DN2l7JzZw1DirVYAdh54Oj5owBq_9CV_cMStpRhy0YCgwYE4gaZP3yDP1gRiy-iJUgfsTNmrVtCF9BEjPCT4iSnDKNxrRRxCqMOJPvodlGJF8qIMn3SvlFqI8Do4zDQmhu8krxN4eDeVhyCJkgNiuTHFvFGu85eEDzEMjyVcNVuXeUSV8BIz14gCfwE52BFisCeoJiOByMY5E1vssZmxH3EdyueobdvSkh1y3fHWYHEGgqrcVR5XpvErYydobHL_oU8xGf5GRxNPL-1APz-sQsScFNPsZ1mPYdejT3xdA5up6p4BTNjPIAZQwFA'
+
+export const environment = {
+  production: true,
+  login: `${urlApiMq}/login`,
+  home: `${urlApiReportes}/ReporteHome`,
+  categories: `${urlApiReportes}/ReporteCategorias`,
+  region: `${urlApiReportes}/ReportePorRegion`,
+  competencies: `${urlApiReportes}/ReportePremiun`,
+  report: `${urlApiReportes}/ReporteDescarga`,
+  tokenMq,
+  config: {
+    interceptor: {
+      header: 'Authorization_APP',
+      token: TOKEN,
+      headerJwt: 'Authorization',
+    },
+  },
+};
