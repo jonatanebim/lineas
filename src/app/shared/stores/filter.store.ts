78cf666092fdbbb0883f010405ffdf7ef494c9b8
Jonatan Iparraguirre <jonatan.iparraguirre-EBIM@quimicasuiza.com>
2024-08-01 10:15:39 -0500

    fix: dist

--- src/app/shared/stores/filter.store.ts
+++ src/app/shared/stores/filter.store.ts
@@ -21,8 +21,10 @@ export class FilterStoreService {
     if (this.months.length !== 6) {
       for (let i = 1; i <= 6; i++) {
         const today = new Date()
+        today.setHours(24);
         today.setMonth(today.getMonth() - i)
         today.setDate(1)
+
         this.months.push({
           value: today.getTime(),
           formated: moment(today).format('YYYY-MM-DD'),
